db59aebf7424892a4274ee8b22f6b9c5062ff129
orololuwa <emmybest.ea@gmail.com>
2023-09-21 21:22:52 +0100

    fix: setup-complete guard error on page refersh

--- src/core/guards/setup-complete.guard.tsx
+++ src/core/guards/setup-complete.guard.tsx
@@ -1,13 +1,17 @@
 import { useToast } from "@chakra-ui/react";
 import { Loading } from "core/components/loading";
-import { useAppSelector } from "core/hooks/use-redux";
+import { useAppDispatch, useAppSelector } from "core/hooks/use-redux";
 import { appRoutes } from "core/routes/routes";
-import { useEffect } from "react";
+import { LocalStorage } from "lib/utils";
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { getARestaurant } from "store/action-creators/restaurant.action";
 
 const SetupCompleteGuard = ({ children }: { children: JSX.Element }) => {
   const data = useAppSelector((state) => state.restaurant.restaurant);
+  const restaurantId = LocalStorage.get("selectedRestaurant");
   const toast = useToast();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (data.error)
@@ -19,10 +23,23 @@ const SetupCompleteGuard = ({ children }: { children: JSX.Element }) => {
   }, [data.error]);
 
   const setupComplete = data?.data?.setupComplete;
+  const [loading, setLoading] = useState(true);
 
-  if (data.loading) return <Loading />;
+  useEffect(() => {
+    if (data.data || !restaurantId) {
+      setLoading(false);
+      return;
+    }
 
-  console.log("@setup_complete", { setupComplete });
+    (async () => {
+      await dispatch(getARestaurant(restaurantId));
+      setLoading(false);
+    })();
+  }, [data]);
+
+  console.log({ loading, setupComplete, "data.loading": data.loading, data });
+
+  if (data.loading || loading) return <Loading />;
 
   if (!setupComplete) {
     return <Navigate to={appRoutes.RESTAURANT_SETUP} />;
